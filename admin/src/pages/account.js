8657561730248110ecf183dd59f9d3591a1a177c
Daniel Santos <daniel.santos@rupealgroup.onmicrosoft.com>
2020-08-09 13:11:30 +0100

    asd

--- admin/src/pages/account.js
+++ admin/src/pages/account.js
@@ -1,6 +1,5 @@
-import React from "react"
-import { Router } from "@reach/router"
-import { login, logout, isAuthenticated, getProfile } from "../utils/auth"
+import React, { Component } from 'react';
+import { login, isAuthenticated, getProfile } from "../utils/auth";
 
 import discount from './icons/002-discount.svg';
 import collect from './icons/039-openbox.svg';
@@ -14,103 +13,118 @@ import '../app.css';
 
 import SideNavigation from './sideNavigation';
 import ChartPage from './chart';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+
+import { connect } from 'react-redux';
+import { getUsersCount } from '../state/actions/data';
 
 
-const Home = ({ user }) => {
-  return <p>Hi, {user.name ? user.name : "friend"}!</p>
-}
+class Account extends Component {
 
-const Account = () => {
-  if (!isAuthenticated()) {
-    login()
-    return <div>
-      {/* <div class="loadingio-spinner-rolling-k8fitpcp7"><div class="ldio-inawj1mgkxk">
-        <div></div>
-      </div></div> */}
-    </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 530,
+      returned: 230,
+    }
   }
 
-  const user = getProfile()
+  componentDidMount() {
+    this.props.getUsersCount();
+  }
 
-  return (
-    <>
-      <div>
-        <Navbar></Navbar>
-        <SideNavigation></SideNavigation>
-        <div className="info-screen">
-          <h1 className="dashboard-title">Analytics</h1>
-          <div className="widget-section">
-            <div className="widget-section__single-widget">
-              <div className="icon-section">
-                <img src={boxes} className="comma-logo" alt="logo" />
+
+  render() {
+    if (!isAuthenticated()) {
+      login()
+      return <div class="center-me-auth0"><div class="loadingio-spinner-rolling-k8fitpcp7"><div class="ldio-inawj1mgkxk"><div></div></div></div></div>
+    }
+
+    const user = getProfile();
+
+    return (
+      <>
+        <div>
+          <Navbar></Navbar>
+          <SideNavigation></SideNavigation>
+          <div className="info-screen">
+            <h1 className="dashboard-title">Analytics</h1>
+            <div className="widget-section">
+              <div className="widget-section__single-widget">
+                <div className="icon-section">
+                  <img src={boxes} className="comma-logo" alt="logo" />
+                </div>
+                <div className="text-section">
+                  <h2 className="small-title">Total Items</h2>
+                  <p className="info">{this.state.total}</p>
+                </div>
               </div>
-              <div className="text-section">
-                <h2 className="small-title">Total Items</h2>
-                <p className="info">530</p>
+
+              <div className="widget-section__single-widget">
+                <div className="icon-section">
+                  <img src={collect} className="comma-logo" alt="logo" />
+                </div>
+                <div className="text-section">
+                  <h2 className="small-title">Returned</h2>
+                  <p className="info">{this.state.returned}</p>
+                </div>
               </div>
+
+              <div className="widget-section__single-widget">
+                <div className="icon-section">
+                  <img src={rreturn} className="comma-logo" alt="logo" />
+                </div>
+                <div className="text-section">
+                  <h2 className="small-title">Already Took</h2>
+                  <p className="info">230</p>
+                </div>
+              </div>
+
+              <div className="widget-section__single-widget">
+                <div className="icon-section">
+                  <img src={discount} className="comma-logo" alt="logo" />
+                </div>
+                <div className="text-section">
+                  <h2 className="small-title">% Returned</h2>
+                  <p className="info">{'%' + ' ' + Math.round(this.state.returned / this.state.total * 100)}</p>
+                </div>
+              </div>
+
+              <div className="widget-section__single-widget">
+                <div className="icon-section">
+                  <img src={userr} className="comma-logo" alt="logo" />
+                </div>
+                <div className="text-section">
+                  <h2 className="small-title">nº Users</h2>
+                  <p className="info">{this.props.numberOfUsers}</p>
+                </div>
+              </div>
+
+              <div className="widget-section__single-widget">
+                <div className="icon-section">
+                  <img src={returnedToday} className="comma-logo" alt="logo" />
+                </div>
+                <div className="text-section">
+                  <h2 className="small-title">Returned today</h2>
+                  <p className="info">30</p>
+                </div>
+              </div>
+
             </div>
-
-            <div className="widget-section__single-widget">
-              <div className="icon-section">
-                <img src={collect} className="comma-logo" alt="logo" />
+            <div className="chart-section">
+              <div className="chart-section__chart">
+                <ChartPage></ChartPage>
               </div>
-              <div className="text-section">
-                <h2 className="small-title">Returned</h2>
-                <p className="info">230</p>
-              </div>
-            </div>
-
-            <div className="widget-section__single-widget">
-              <div className="icon-section">
-                <img src={rreturn} className="comma-logo" alt="logo" />
-              </div>
-              <div className="text-section">
-                <h2 className="small-title">Already Took</h2>
-                <p className="info">230</p>
-              </div>
-            </div>
-
-            <div className="widget-section__single-widget">
-              <div className="icon-section">
-                <img src={discount} className="comma-logo" alt="logo" />
-              </div>
-              <div className="text-section">
-                <h2 className="small-title">% Returned</h2>
-                <p className="info">230</p>
-              </div>
-            </div>
-
-            <div className="widget-section__single-widget">
-              <div className="icon-section">
-                <img src={userr} className="comma-logo" alt="logo" />
-              </div>
-              <div className="text-section">
-                <h2 className="small-title">nº Users</h2>
-                <p className="info">1130</p>
-              </div>
-            </div>
-
-            <div className="widget-section__single-widget">
-              <div className="icon-section">
-                <img src={returnedToday} className="comma-logo" alt="logo" />
-              </div>
-              <div className="text-section">
-                <h2 className="small-title">Returned today</h2>
-                <p className="info">30</p>
-              </div>
-            </div>
-
-          </div>
-          <div className="chart-section">
-            <div className="chart-section__chart">
-              <ChartPage></ChartPage>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
-export default Account
+const mapStateToProps = state => ({
+  numberOfUsers: state.dashboard.numberOfUsers
+})
+
+export default connect(mapStateToProps, { getUsersCount })(Account);
--- admin/src/state/actions/data.js
+++ admin/src/state/actions/data.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 import {
   GET_QRCODE_RETURN,
-  GET_QRCODE_COLLECT
+  GET_QRCODE_COLLECT,
+  GET_USERS_COUNT
 } from './types';
 
 // Get Qr Code number -->  collect 
@@ -26,6 +27,20 @@ export const getQrCodeReturn = () => async dispatch => {
     dispatch({
       type: GET_QRCODE_RETURN,
       payload: res.data
+    });
+  } catch (err) {
+      console.log(err)
+  }
+};
+
+// Get Qr Code return number --> return
+export const getUsersCount = () => async dispatch => {
+  console.log('getUsersdata called action')
+  try {
+    const res = await axios.get('https://recollect0.herokuapp.com/users/');
+    dispatch({
+      type: GET_USERS_COUNT,
+      payload: res.data.count
     });
   } catch (err) {
       console.log(err)
